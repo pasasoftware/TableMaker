dd24044d30dfd736423a804c4a432654d97eaf6f
ChengDong <chengdong_dev@163.com>
2025-07-18 16:54:04 +0800

    Fix package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "TableMaker",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
